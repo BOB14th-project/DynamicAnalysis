63519cf352a69f0727d0f3caeb8d12bb7fa6e257
noir1458 <noir1458@gmail.com>
2025-09-25 17:58:18 +0900

    chore: stop tracking build/ (use .gitignore)

--- build/CMakeFiles/hook.dir/src/resolver.cpp.o.d
+++
@@ -1,7 +0,0 @@
-CMakeFiles/hook.dir/src/resolver.cpp.o: \
- /mnt/d/git/Hooking_linux_LD_PRELOAD/src/resolver.cpp \
- /usr/include/stdc-predef.h \
- /mnt/d/git/Hooking_linux_LD_PRELOAD/include/resolver.h \
- /mnt/d/git/Hooking_linux_LD_PRELOAD/include/hook_common.h \
- /mnt/d/git/Hooking_linux_LD_PRELOAD/include/log.h \
- /usr/lib/gcc/x86_64-linux-gnu/13/include/stddef.h
